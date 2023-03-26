ddb389464786a7779d507f545ee2afadf6d719a4
ゆっくり <116621498+sekand@users.noreply.github.com>
2023-03-26 14:13:10 +0000

    Initial commit

---
+++ Phigros.py
@@ -0,0 +1,95 @@
+# Phigrosの楽曲情報をリストで定義する
+phigros_songs = [
+    {
+        'title': 'Poison AND÷OR Affection',
+        'bpm': 120,
+        'difficulty': {
+            'EZ': {
+                'level': 3.5,
+                'combo': 140,
+                'charter':"R/kko",
+            },
+            'HD': {
+                'level': 9.6,
+                'combo': 278,
+                'charter':"&ikko"
+            },
+            'IN': {
+                'level': 15.5,
+                'combo': 888,
+                'charter':"1÷98"
+            }
+        },
+        'composer': 'Leaf',
+        'song chapter': 'シングル 精選集'
+    },
+    {
+        'title': 'インフェルノシティ',
+        'bpm': 215,
+        'difficulty': {
+            'EZ': {
+                'level': 5.0,
+                'combo': 352,
+                'charter':"Su1fuR",
+            },
+            'HD': {
+                'level': 11.2,
+                'combo': 578,
+                'charter':"Alloces feat. Uvernight",
+            },
+            'IN': {
+                'level': 15.7,
+                'combo': 1275,
+                'charter':"Alloces",
+            }
+        },
+        'composer': 'Ponchi♪ feat. はぁち',
+        'song chapter': 'シングル 精選集'
+},
+    {
+        'title': 'Upshift',
+        'bpm': 155,
+        'difficulty': {
+            'EZ': {
+                'level': 5.5,
+                'combo': 630,
+                'charter':"Lumiere",
+            },
+            'HD': {
+                'level': 10.8,
+                'combo': 545,
+                'charter':"CorruptedEntity&野从",
+            },
+            'IN': {
+                'level': 15.3,
+                'combo': 946,
+                'charter':"_鉄",
+            }
+        },
+        'composer': 'Iriss-Frantzz',
+        'song chapter': 'シングル 精選集'
+},    
+  {
+        'title': '青丘',
+        'bpm': 180,
+        'difficulty': {
+            'EZ': {
+                'level': 5.5,
+                'combo': 284,
+                'charter':"Ctymax",
+            },
+            'HD': {
+                'level': 10.5,
+                'combo': 534,
+                'charter':"Su1fuR",
+            },
+            'IN': {
+                'level': 15.3,
+                'combo': 1304,
+                'charter':"KillJoy",
+            }
+        },
+        'composer': 'BLACK 0',
+        'song chapter': 'シングル 精選集'
+},
+]
---
+++ main.py
@@ -0,0 +1,41 @@
+import keep_alive
+from discord import app_commands
+import discord
+from Phigros import phigros_songs
+keep_alive.keep_alive()
+intents = discord.Intents.all()
+client=discord.Client(intents=intents)
+tree= app_commands.CommandTree(client)
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game(name="Phigros"))
+
+    await tree.sync()
+
+
+#楽曲入力やっとけ
+@tree.command(name="phigros")
+async def Phigros(ctx, *, song_name:str):
+    for song in phigros_songs:
+        if song['title'].lower() == song_name.lower():
+            # Embedを作成して、情報を追加する
+            embed = discord.Embed(title=song['title'], color=discord.Color.green())
+            embed.add_field(name='BPM', value=song['bpm'], inline=False)
+            for diff in song['difficulty']:
+                embed.add_field(name=diff, value=f"Level: {song['difficulty'][diff]['level']}, Combo: {song['difficulty'][diff]['combo']}, Charter:{song['difficulty'][diff]['charter']}",inline=False)
+            embed.add_field(name='Composer', value=song['composer'], inline=False)
+            embed.add_field(name='Song chapter', value=song['song chapter'], inline=False)
+            await ctx.response.send_message(embed=embed)
+            return
+
+    # 楽曲が見つからない場合はエラーメッセージを表示する
+    await ctx.response.send_message('楽曲が見つかりませんでした。')
+
+#ACC計算用100%超えないようにするのは後でやる
+@tree.command(name="acc",description="p,パーフェクト数、g,グッド数、tn,総ノーツ数")
+async def acc(ctx,*, p: int, g: int, tn: int):
+    acc = (p + g * 0.65) / tn * 100
+    await ctx.response.send_message(f"Accuracy: {acc:.2f}%")
+
+client.run('MTA3NjQwNzU5Mjc2ODc3NDIxNA.Gjytnl.JOjJ3mPxxf1yrdbD71QHnEiTeeTjx7puyQAFbs')
